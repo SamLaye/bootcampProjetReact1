2ab995caadaceee79b0819a9384d6cf8ffc6d571
Sam Laye <layesamss97@gmail.com>
2023-11-22 16:52:55 +0000

    Correction sur Home et Card pour la gestion de l'affichage

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import user from '../assets/user.png'
 
+
 const Navbar = () => {
     return ( 
         <div className='row justify-content-between py-2'>
